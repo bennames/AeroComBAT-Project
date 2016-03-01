9247000a8a044e0768e2996b22f327eb43d8b07d
adamzelenka <zadam7@vt.edu>
2016-03-01 11:04:18 -0500

    Unit Testing
    trying to make functional unit tests

--- AeroComBAT/StructuresTest.py
+++ AeroComBAT/StructuresTest.py
@@ -8,68 +8,68 @@ import unittest
 
 from Structures import Node
 
+
+
 class NodeTest(unittest.TestCase):
     """Creates a test for the Node class.
     """
     def __init__(self):
-        """Constructor does nothing and is therefore left blank.
+        """Constructor declares Node object for testing.
         """
-        #global myNode;
         self.myNode = None
-        
+    
     def setUp3(self):
         """Creates a fresh Node object with three defined coordinates
         """
-        self.myNode = Node(0, [1.,2.,3.]);
+        self.myNode = Node(0, [1.,2.,3.])
         
     def setUp2(self):
         """Creates a fresh Node object with two defined coordinates
         """
-        self.myNode = Node(0, [1.,2.]);
+        self.myNode = Node(0, [1.,2.])
     
     def setUp1(self):
         """Creates a fresh Node object with one defined coordinate
         """
-        self.myNode = Node(0, [1.]);
+        self.myNode = Node(0, [1.])
         
     def setUp0(self):
         """Creates a fresh Node object with no defined coordinates
         """
-        self.myNode = Node(0, []);
+        self.myNode = Node(0, [])
         
         
     def testLengths(self):
         """Tests to see if created Nodes have correctly defined coordinates
         """
-        self.setUp0();
-        self.assertEqual([0.,0.,0.], self.myNode.x);
-        self.setUp1();
-        self.assertEqual([1.,0.,0.], self.myNode.x);
-        self.setUp2();
-        self.assertEqual([1.,2.,0.], self.myNode.x);
-        self.setUp3();
-        self.assertEqual([1.,2.,3.], self.myNode.x);
-        
+        self.setUp0()
+        self.assertEqual([0.,0.,0.], self.myNode.x)
+        self.setUp1()
+        self.assertEqual([1.,0.,0.], self.myNode.x)
+        self.setUp2()
+        self.assertEqual([1.,2.,0.], self.myNode.x)
+        self.setUp3()
+        self.assertEqual([1.,2.,3.], self.myNode.x)
+    
     def testSetUpErrors(self):
         """Tests to see if the Node class will create a Node object with a 
            non-integer Node ID
         """
         while True:
             try:
-                self.myNode = Node('a', []);
-                break;
+                self.myNode = Node('a', [])
+                break
             except TypeError as e:
-                print(e);
+                print(e)
                 
         while True:
             try:
-                self.myNode = Node(1, [1.,2.,3.,4.]);
-                break;
+                self.myNode = Node(1, [1.,2.,3.,4.])
+                break
             except ValueError as e:
-                print(e);
-        return;
+                print(e)
                 
-    
+if __name__ == '__main__': unittest.main()
                 
 
            
